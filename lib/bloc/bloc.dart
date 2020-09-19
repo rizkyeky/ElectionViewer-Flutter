00371fbc40f0c7391fe6fc8314f07740eae4447c
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-19 17:47:38 +0700

    add packages

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -1,6 +1,12 @@
 library bloc;
 
+import 'package:rxdart/rxdart.dart';
+
+import 'package:pemilihan_app/locator.dart';
+import 'package:pemilihan_app/service/service.dart';
+
 part 'bloc_typeselection.dart';
+part 'bloc_loginadmin.dart';
 
 abstract class Bloc {
   void init();
