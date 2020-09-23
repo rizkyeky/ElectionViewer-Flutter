ea038430faed97655e5d3b3bd483653d16bdae72
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-23 17:37:06 +0700

    update member

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -1,5 +1,7 @@
 // import 'dart:convert';a
 
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart' as fire_auth;
 import 'package:cloud_firestore/cloud_firestore.dart'  as fire_store;
 import 'package:firebase_core/firebase_core.dart' as fire_core;
@@ -12,3 +14,4 @@ part 'service_core.dart';
 part 'service_auth.dart';
 part 'service_user.dart';
 part 'service_calon.dart';
+part 'service_kecamatan.dart';
