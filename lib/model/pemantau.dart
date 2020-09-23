384206965cfee3754a4cb77a241e20f191a6b0fc
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-23 12:54:59 +0700

    fix and update pemantau

--- lib/model/pemantau.dart
+++ lib/model/pemantau.dart
@@ -1,11 +1,30 @@
 part of 'model.dart';
 
-class Pemantau {
-  String id;
-  String email;
-  String nama;
+class Pemantau implements User {
   
-  Pemantau(this.id, this.nama, this.email);
+  @override
+  String email;
 
+  @override
+  String id;
+
+  @override
+  String name;
+
+
+  Pemantau(this.id, this.name, this.email);
   factory Pemantau.initial() => Pemantau('', '', '');
+
+  @override
+  void copyWith({String id, String name, String email}) {
+  }
+  
+  @override
+  void duplicate(User user) {
+    id = user.id;
+    name = user.name;
+    email = user.email;
+  }
+
+  
 }
