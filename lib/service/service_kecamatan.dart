43ee12ec994c062a87bbd4459a8e3115d09a8562
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-23 17:36:55 +0700

    add kecamatan service

---
+++ lib/service/service_kecamatan.dart
@@ -0,0 +1,34 @@
+part of 'service.dart';
+
+class KecamatanService {
+  final http.Client _client = http.Client();
+  final String _homeBase = 'dev.farizdotid.com';
+  
+  Future<List<Kecamatan>> getKecamatans() async {
+    try {
+      const String _subBase = 'api/daerahindonesia/kecamatan';
+
+      final Uri _uri = Uri.https(_homeBase, _subBase, {'id_kota': '7271'});
+      final http.Response response = await _client.get(_uri)
+        .timeout(const Duration(seconds: 10));
+
+      if (response.statusCode != 200) {
+        // Respone not success
+        return [];
+      }
+      else {
+        // Respone success
+        final Map data = json.decode(response.body) as Map;
+        final List result = data['kecamatan'] as List;
+        
+        // print('respone success');
+        
+        return result.map<Kecamatan>((e) => Kecamatan
+          .fromJson(e as Map<String, dynamic>)).toList();
+      }
+    } catch (e) {
+      // print('request was failed');
+      return [];
+    }
+  }
+}
