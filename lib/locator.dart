dcc7f5caf773227651710411b2b47310c4e2540f
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-23 12:53:09 +0700

    add and fix locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
 
 import 'bloc/bloc.dart';
-// import 'model/model.dart';
+import 'model/model.dart';
 import 'service/service.dart';
 
 GetIt locator = GetIt.instance;
@@ -11,8 +11,8 @@ void setupLocator() {
   // Waiting for first launch
   locator.registerLazySingleton(() => CoreService());
   locator.registerLazySingleton(() => AuthService());
-  locator.registerLazySingleton(() => PemantauService());
-  // locator.registerLazySingleton(() => MovieService(), instanceName: 'Movie Service');
+  locator.registerLazySingleton(() => UserService.admin(), instanceName: 'Service Admin');
+  locator.registerLazySingleton(() => UserService.pemantau(), instanceName: 'Service Pemantau');
   // locator.registerLazySingleton(() => LocalService(), instanceName: 'Local Service');
   // locator.registerLazySingleton(() => TicketService(), instanceName: 'Ticket Service');
   // locator.registerLazySingleton(() => TransactionService(), instanceName: 'Transaction Service');
@@ -41,12 +41,8 @@ Future<void> awaitSetupLocator() async {
   await locator.allReady();
 
   // Setup service
-  await locator.get<CoreService>(instanceName: 'Core Service').init();
-  await locator.get<AuthService>(instanceName: 'Auth Service').init();
-  // await locator.get<LocalService>(instanceName: 'Local Service').init();
-
-  // Setup first bloc
-  // await locator.get<HomeBloc>().init();
+  await locator<CoreService>().init();
+  await locator<AuthService>().init();
 
   // print("setup locator success");
 }
