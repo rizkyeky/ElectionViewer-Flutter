69273aec5d505919d080702e651cb5806552e00d
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-23 12:53:59 +0700

    fix and update admin model

--- lib/model/admin.dart
+++ lib/model/admin.dart
@@ -1,11 +1,30 @@
 part of 'model.dart';
 
-class Admin {
-  String id;
-  String email;
-  String nama;
+class Admin implements User {
   
-  Admin(this.id, this.email);
+  @override
+  String email;
 
-  factory Admin.initial() => Admin('', '');
+  @override
+  String id;
+
+  @override
+  String name;
+  
+  Admin(this.id, this.name, this.email);
+  factory Admin.initial() => Admin('', '', '');
+
+  @override
+  void copyWith({String id, String name, String email}) {
+    this.id = id ?? this.id;
+    this.name = name ?? this.name;
+    this.email = email ?? this.email;
+  }
+  
+  @override
+  void duplicate(User user) {
+    id = user.id;
+    name = user.name;
+    email = user.email;
+  }
 }
