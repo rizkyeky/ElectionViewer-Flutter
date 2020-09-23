c0c8380e38d1f83129424b52d66281ca8f7581d6
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-23 21:51:41 +0700

    fix and update pemilihan bloc

--- lib/bloc/bloc_pemilihantempat.dart
+++ lib/bloc/bloc_pemilihantempat.dart
@@ -2,23 +2,90 @@ part of 'bloc.dart';
 
 class PemilihanTempatBloc implements Bloc {
 
-  final List<String> _kecamatan = List.generate(5, (index) => 'Kec$index');
-  List<String> get kecamatan => _kecamatan;
+  final BehaviorSubject<Map<String, List>> _pemilihanController = BehaviorSubject();
+  Stream<Map<String, List>> get pemilihanStream => _pemilihanController.stream;
 
-  final List<String> _kelurahan = List.generate(5, (index) => 'Kel$index');
-  List<String> get kelurahan => _kelurahan;
+  final KecamatanService _kecamatanService = locator.get<KecamatanService>(
+    instanceName: 'Service Kecamatan');
+  final KelurahanService _kelurahanService = locator.get<KelurahanService>(
+    instanceName: 'Service Kelurahan');
 
-  final List<String> _tps = List.generate(5, (index) => 'TPS$index');
-  List<String> get tps => _tps;
+  List<Kecamatan> _kecamatans = [];
+  List<Kecamatan> get kecamatans => _kecamatans;
+
+  List<Kelurahan> _kelurahans = [];
+  List<Kelurahan> get kelurahans => _kelurahans;
+  
+  List<String> _tpses = [];
+  List<String> get tpses => _tpses;
+
+  final Map<String, dynamic> _selectedTempat = {};
+  Map<String, dynamic> get selectedTempat => _selectedTempat;
+  
+  final Map<String, List> pemilihan = const {
+    'kecamata': [],
+    'kelurahan': [],
+    'tps': []
+  };
+
+  @override
+  Future<void> init() async {
+    await getKecamatans();
+  }
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    _pemilihanController.close();
   }
 
-  @override
-  void init() {
-    // TODO: implement init
+  void selectTempat({int indexKec, int indexKel, int indexTPS}) {
+
+    if (indexKec != null) {
+      _selectedTempat['kecamatan'] = _kecamatans[indexKec];
+      _selectedTempat.remove('kelurahan');
+      _selectedTempat.remove('tps');
+    }
+    if (indexKel != null) {
+      _selectedTempat['kelurahan'] = _kelurahans[indexKel];
+      _selectedTempat.remove('tps');
+    }
+    if (indexTPS != null) _selectedTempat['tps'] = _tpses[indexTPS];
+
+    print(_selectedTempat);
   }
 
+  void updatePemilihanStream({List kec, List kel, List tps}) {
+    final Map<String, List> data = {
+      'kecamatan': _kecamatans,
+      'kelurahan': _kelurahans,
+      'tps': _tpses
+    };
+
+    if (kec != null) data['kecamatan'] = kec;
+    if (kel != null) data['kelurahan'] = kel;
+    if (tps != null) data['tps'] = tps;
+
+    _pemilihanController.sink.add(data);
+  }
+
+ Future<void> getKecamatans() async {
+   await _kecamatanService.getKecamatans()
+   .then((list) {
+    _kecamatans = list;
+    updatePemilihanStream(kec: _kecamatans);
+   });
+ }
+ 
+ Future<void> getKelurahans(int idKec) async {
+   await _kelurahanService.getKelurahans(idKec)
+    .then((list) {
+      _kelurahans = list;
+      updatePemilihanStream(kel: _kelurahans);
+    });
+ }
+
+ void generateTPSes() {
+  _tpses = List.generate(5, (index) => 'TPS 0${index+1}');
+  updatePemilihanStream(tps: _tpses);  
+ }
 }
