5516f552f5b7a6a58b2edd88a8dfcaac71e83639
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-24 20:38:08 +0700

    update calon model

--- lib/model/calon.dart
+++ lib/model/calon.dart
@@ -4,9 +4,32 @@ class Calon {
   String id;
   String name;
   int number;
-  int suara;
+  int totalSuara;
+  int sahSuara;
+  int tidaksahSuara;
   String photoURL;
 
-  Calon(this.id, this.name, this.number, {this.suara = 0, this.photoURL = ''});
+  Calon(this.id, this.name, this.number, {
+    this.totalSuara = 0, 
+    this.sahSuara = 0, 
+    this.tidaksahSuara = 0, 
+    this.photoURL = ''
+  });
   factory Calon.initial() => Calon('', '', 0);
+
+  void copyWith({
+    String id,
+    String name,
+    int number,
+    int totalSuara,
+    int sahSuara,
+    int tidaksahSuara,    
+  }) {
+    this.id = id ?? this.id;
+    this.name = name ?? this.name;
+    this.number = number ?? this.number;
+    this.totalSuara = totalSuara ?? this.totalSuara;
+    this.sahSuara = sahSuara ?? this.sahSuara;
+    this.tidaksahSuara = tidaksahSuara ?? this.tidaksahSuara;
+  }
 }
