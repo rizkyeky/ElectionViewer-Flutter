4a764f7ee9b656b191c16f6fc8d64cdf42385496
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-24 11:25:45 +0700

    add and update input data bloc

--- lib/bloc/bloc_inputdata.dart
+++ lib/bloc/bloc_inputdata.dart
@@ -1,14 +1,30 @@
 part of 'bloc.dart';
 
 class InputDataBloc implements Bloc {
-  @override
-  void dispose() {
-    // TODO: implement dispose
-  }
+
+  final CalonService _calonService = locator
+    .get<CalonService>(instanceName: 'Service Calon');
+  final SuaraService _suaraService = locator
+    .get<SuaraService>(instanceName: 'Service Suara');
+
+  final List<Calon> _calons = [];
+  List<Calon> get calons => _calons;
 
   @override
-  void init() {
-    // TODO: implement init
+  void dispose() {}
+
+  @override
+  Future<void> init() async {
+    // await getCalons();
   }
 
+  Future<Calon> getCalon(int index) async {
+    _calons.addAll(await _calonService.getCalons());
+    return _calons[index];
+  }
+
+  Stream<int> getSuaraCalon(int index) {
+    return _calonService.getCalon(_calons[index].id).asStream()
+      .map((event) => event.suara); 
+  }
 }
