7daf77dbef0030fcdf56147ae5fe67fc5aceb82d
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-24 11:27:18 +0700

    fix and update calon service

--- lib/service/service_calon.dart
+++ lib/service/service_calon.dart
@@ -31,12 +31,31 @@ class CalonService {
     await _calonCollection.doc(id).update(data);
   }
 
-  Future<Calon> getCalon(String id) async {
-    final fire_store.DocumentSnapshot snapshot = await _calonCollection.doc(id).get();
+  Future<List<Calon>> getCalons() async {
+    final fire_store.QuerySnapshot snapshot = await _calonCollection.get();
+    
+    final List<Calon> data = snapshot.docs.map((e) => Calon(
+      e.id,
+      e.data()["nama"] as String,
+      (e.data()["nomor"] as num).toInt(),
+      suara: (e.data()["suara"] as num).toInt(),
+      photoURL: e.data()["photoURL"] as String,
+    )).toList();
 
+    data.sort((calon1, calon2) => calon1.number.compareTo(calon2.number));
+    return data;
+  }
+
+  Future<Calon> getCalon(String id) async {
+    final fire_store.DocumentSnapshot snapshot = await _calonCollection
+      .doc(id).get();
+    
     return Calon(
       id,
       snapshot.data()["nama"] as String,
+      (snapshot.data()["nomor"] as num).toInt(),
+      suara: (snapshot.data()["suara"] as num).toInt(),
+      photoURL: snapshot.data()["photoURL"] as String,
     );
   }
 }
