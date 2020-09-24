302b866c3014518112f135f9b72b7c2fd6450dd2
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-24 22:12:29 +0700

    fix and update input data page

--- lib/page/page_inputdata.dart
+++ lib/page/page_inputdata.dart
@@ -20,55 +20,55 @@ class InputDataPage extends Page<InputDataBloc> {
       child: FutureBuilder<Calon>(
         future: bloc.getCalon(index),
         builder: (context, snapshotFuture) => (snapshotFuture.hasData) ? Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Calon $index', style: blackSubtitleBold,),
-                      Text(snapshotFuture.data.name, style: blackSubtitleRegular,),
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text('Total Suara', style: blackContentRegular,),
-                      StreamBuilder<int>(
-                        initialData: snapshotFuture.data.totalSuara,
-                        stream: bloc.getSuaraCalon(index),
-                        builder: (context, snapshotStream) {
-                          return Text(convertCurr(snapshotStream.data), style: blackNumber,);
-                        }
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  XTextField(
-                    keyboardType: TextInputType.number,
-                    onChanged: (value) {
-                      bloc.inputSahSuara(index, int.parse(value));
-                    },
-                    text: 'Suara Sah',
-                  ),
-                  const SizedBox(height: 15,),
-                  XTextField(
-                    keyboardType: TextInputType.number,
-                    onChanged: (value) {
-                      bloc.inputTidakSahSuara(index, int.parse(value));
-                    },
-                    text: 'Suara Tidak Sah',
-                  ),
-                ],
-              )
-            ],
-          ) : const Center(child: CircularProgressIndicator(),)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Calon $index', style: blackSubtitleBold,),
+                    Text(snapshotFuture.data.name, style: blackSubtitleRegular,),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text('Total Suara', style: blackContentRegular,),
+                    StreamBuilder<int>(
+                      initialData: snapshotFuture.data.totalSuara,
+                      stream: bloc.getSuaraCalon(index),
+                      builder: (context, snapshotStream) {
+                        return Text(convertCurr(snapshotStream.data), style: blackNumber,);
+                      }
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                XTextField(
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    bloc.inputSahSuara(index, int.parse(value));
+                  },
+                  text: 'Suara Sah',
+                ),
+                const SizedBox(height: 15,),
+                XTextField(
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    bloc.inputTidakSahSuara(index, int.parse(value));
+                  },
+                  text: 'Suara Tidak Sah',
+                ),
+              ],
+            )
+          ],
+        ) : const Center(child: CircularProgressIndicator(),)
       ),
     );
   }
@@ -87,12 +87,24 @@ class InputDataPage extends Page<InputDataBloc> {
           children: List<Widget>.generate(2, (index) => buildInputBox(context, index+1))
         ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: mainColor,
-        onPressed: () => bloc.sendSuara(), 
-        label: Text('Kirim', style: whiteSubtitleBold,),
-        elevation: 0,
-        highlightElevation: 0,
+      floatingActionButton: Builder(
+        builder: (context) => FloatingActionButton.extended(
+          backgroundColor: mainColor,
+          onPressed: () async{
+            if (bloc.hakInputPemantau) {
+              await bloc.sendSuara();
+            } else {
+              Scaffold.of(context).showSnackBar(snackBar(
+                contentText: 'Pemantau sudah mengirim suara',
+                labelText: 'TUTUP',
+                onPressed: () => Scaffold.of(context).hideCurrentSnackBar()
+              ));
+            }
+          }, 
+          label: Text('Kirim', style: whiteSubtitleBold,),
+          elevation: 0,
+          highlightElevation: 0,
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
