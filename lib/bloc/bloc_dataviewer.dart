ba605c359264ada02f4b9b6e4c98441821da1af7
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-25 19:02:08 +0700

    add and update data viewer

--- lib/bloc/bloc_dataviewer.dart
+++ lib/bloc/bloc_dataviewer.dart
@@ -1,12 +1,11 @@
 part of 'bloc.dart';
 
-
 class DataViewerBloc implements Bloc {
+
+  final CalonService _calonService = locator
+    .get<CalonService>(instanceName: 'Service Calon');
   
-  final List<Calon> _calons = [
-    Calon('1', 'Eky', 1),
-    Calon('2', 'Rizky', 2),
-  ];
+  final List<Calon> _calons = [];
   List<Calon> get calons => _calons; 
   
   @override
@@ -14,5 +13,34 @@ class DataViewerBloc implements Bloc {
 
   @override
   void init() {}
+
+  Future<List<Calon>> getCalons() async {
+    _calons.addAll(await _calonService.getCalons(encrypt: false));
+    return _calons;
+  }
+
+  int totalSuara() {
+    int temp = 0;
+    for (final Calon calon in _calons) {
+      temp += calon.totalSuara;
+    }
+    return temp;
+  }
+
+  int totalSuaraSah() {
+    int temp = 0;
+    for (final Calon calon in _calons) {
+      temp += calon.sahSuara;
+    }
+    return temp;
+  }
+
+  int totalSuaraTidakSah() {
+    int temp = 0;
+    for (final Calon calon in _calons) {
+      temp += calon.tidaksahSuara;
+    }
+    return temp;
+  }
 
 }
