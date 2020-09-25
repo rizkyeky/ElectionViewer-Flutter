5ff0e285e8e930656959bdf8c256f8617e4235ab
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-25 23:16:50 +0700

    add and update pemantau model

--- lib/model/pemantau.dart
+++ lib/model/pemantau.dart
@@ -12,16 +12,18 @@ class Pemantau implements User {
   String name;
 
   bool hakInput;
+  Map<String, String> tempat;
 
-  Pemantau(this.id, this.name, this.email, {this.hakInput = true});
+  Pemantau(this.id, this.name, this.email, {this.hakInput = true, this.tempat = const {}});
   factory Pemantau.initial() => Pemantau('', '', '');
 
   @override
-  void copyWith({String id, String name, String email, bool hakInput}) {
+  void copyWith({String id, String name, String email, bool hakInput, Map<String, String> tempat}) {
     this.id = id ?? this.id;
     this.name = name ?? this.name;
     this.email = email ?? this.email;
-    this.hakInput = hakInput ?? this.hakInput; 
+    this.hakInput = hakInput ?? this.hakInput;
+    this.tempat = tempat ?? this.tempat;
   }
   
   @override
@@ -30,6 +32,4 @@ class Pemantau implements User {
     name = user.name;
     email = user.email;
   }
-
-  
 }
