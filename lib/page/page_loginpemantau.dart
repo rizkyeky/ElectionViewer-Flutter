0dfa5e4fbeea750b7c8865e72f47d532a40836b5
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-25 23:15:32 +0700

    add and update login pemantau page

--- lib/page/page_loginpemantau.dart
+++ lib/page/page_loginpemantau.dart
@@ -46,36 +46,36 @@ class LogInPemantauPage extends Page<LogInPemantauBloc> {
                 color: mainColor,
                 width: 140,
                 onTap: () async {
-                  _bloc.createPemantau();
-                  // final bool isValidEmail = _emailText.text.contains(
-                  //   RegExp(r'^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$')
-                  // ); 
+                  
+                  final bool isValidEmail = _emailText.text.contains(
+                    RegExp(r'^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$')
+                  ); 
 
-                  // final bool isValidPassword = _passwordText.text.length >= 6; 
+                  final bool isValidPassword = _passwordText.text.length >= 6; 
 
-                  // if (isValidEmail && isValidPassword) {
-                  //   await _bloc.signIn(
-                  //     _emailText.text, _passwordText.text)
-                  //     .then((value) {
-                  //       if (value == 'User Active') {
+                  if (isValidEmail && isValidPassword) {
+                    await _bloc.logIn(
+                      _emailText.text, _passwordText.text)
+                      .then((value) {
+                        if (value == 'User Active') {
                           Navigator.pushReplacementNamed(context, '/pemilihantempat');
-                  //       }
-                  //       else {
-                  //         Scaffold.of(contextScaffold).showSnackBar(snackBar(
-                  //           contentText: value,
-                  //           labelText: 'DISMISS',
-                  //           onPressed: () => Scaffold.of(contextScaffold).hideCurrentSnackBar()
-                  //         ));
-                  //       }
-                  //     }
-                  //   );
-                  // } else {
-                  //   Scaffold.of(contextScaffold).showSnackBar(snackBar(
-                  //     contentText: 'Email or password not valid',
-                  //     labelText: 'DISMISS',
-                  //     onPressed: () => Scaffold.of(contextScaffold).hideCurrentSnackBar()
-                  //   ));
-                  // }
+                        }
+                        else {
+                          Scaffold.of(contextScaffold).showSnackBar(snackBar(
+                            contentText: value,
+                            labelText: 'TUTUP',
+                            onPressed: () => Scaffold.of(contextScaffold).hideCurrentSnackBar()
+                          ));
+                        }
+                      }
+                    );
+                  } else {
+                    Scaffold.of(contextScaffold).showSnackBar(snackBar(
+                      contentText: 'Email or password not valid',
+                      labelText: 'DISMISS',
+                      onPressed: () => Scaffold.of(contextScaffold).hideCurrentSnackBar()
+                    ));
+                  }
                 },
                 child: Text('Masuk', style: whiteSubtitleBold,),
               ),
