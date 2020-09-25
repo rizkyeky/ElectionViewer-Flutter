c2f246e0a812a828e28bbcb41a4fdedbb904e495
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-25 08:49:00 +0700

    finish fixing and update input data bloc

--- lib/bloc/bloc_inputdata.dart
+++ lib/bloc/bloc_inputdata.dart
@@ -2,48 +2,97 @@ part of 'bloc.dart';
 
 class InputDataBloc implements Bloc {
 
+  final BehaviorSubject<List<int>> _countSuaraController = BehaviorSubject();
+  Stream<List<int>> get streamCountSuara => _countSuaraController.stream;
+
+  final BehaviorSubject<bool> _loadingController = BehaviorSubject();
+  Stream<bool> get isLoadingStream => _loadingController.stream;
+
   final CalonService _calonService = locator
     .get<CalonService>(instanceName: 'Service Calon');
-  // final SuaraService _suaraService = locator
-  //   .get<SuaraService>(instanceName: 'Service Suara');
-
+  
   final List<Calon> _calons = [];
   List<Calon> get calons => _calons;
 
+  final List<Map<String, int>> _inputSuaraCalons = [{}];
+
   @override
-  void dispose() {}
+  void dispose() {
+    _countSuaraController.close();
+    _loadingController.close();
+  }
 
   @override
   Future<void> init() async {
-    // await getCalons();
+  }
+
+  Future<List<Calon>> getCalons() async {
+    _calons.addAll(await _calonService.getCalons());
+    
+    _countSuaraController.sink.add(
+      List.generate(_calons.length, (index) => 0)
+    );
+    
+    for (int i = 0; i < _calons.length; i++) {
+      _inputSuaraCalons.add({
+        'total_suara': 0,
+        'suara_sah': 0,
+        'suara_tidak_sah': 0
+      });
+    }
+    
+    return _calons;
   }
 
   Future<Calon> getCalon(int index) async {
-    _calons.addAll(await _calonService.getCalons());
     return _calons[index];
   }
 
-  Stream<int> getSuaraCalon(int index) {
-    return _calonService.streamSuaraCalon(_calons[index].id); 
-  }
-
   void inputSahSuara(int index, int value) {
-    _calons[index].copyWith(sahSuara: value);
+    _inputSuaraCalons[index]['suara_sah'] = value;
+    
+    if (!_inputSuaraCalons[index].containsKey('suara_tidak_sah')) {
+      _inputSuaraCalons[index]['suara_tidak_sah'] = 0;  
+    }
+    
+    _inputSuaraCalons[index]['total_suara'] = 
+    _inputSuaraCalons[index]['suara_sah'] + _inputSuaraCalons[index]['suara_tidak_sah'];  
+    
+    _countSuaraController.sink.add(
+      List.generate(_calons.length, (index) => 
+      _inputSuaraCalons[index]['total_suara'])
+    );
   }
 
   void inputTidakSahSuara(int index, int value) {
-    _calons[index].copyWith(tidaksahSuara: value);
+    _inputSuaraCalons[index]['suara_tidak_sah'] = value;
+   
+    if (!_inputSuaraCalons[index].containsKey('suara_sah')) {
+      _inputSuaraCalons[index]['suara_sah'] = 0;  
+    }
+
+    _inputSuaraCalons[index]['total_suara'] = 
+    _inputSuaraCalons[index]['suara_sah'] + _inputSuaraCalons[index]['suara_tidak_sah'];  
+
+    _countSuaraController.sink.add(
+      List.generate(_calons.length, (index) => 
+      _inputSuaraCalons[index]['total_suara'])
+    );
   }
 
   Future<void> sendSuara() async {
-    for (final Calon calon in _calons) {
-      await _calonService.updateCalon(calon.id,
-        totalSuara: calon.sahSuara + calon.tidaksahSuara, 
-        tidaksahSuara: calon.tidaksahSuara,
-        sahSuara: calon.sahSuara
+    _loadingController.sink.add(true);
+    
+    for (int i = 0; i < _calons.length; i++) {
+      await _calonService.updateCalon(calons[i].id,
+        totalSuara: _inputSuaraCalons[i]['total_suara'], 
+        tidaksahSuara: _inputSuaraCalons[i]['suara_tidak_sah'],
+        sahSuara: _inputSuaraCalons[i]['suara_sah']
       );
     }
+
     locator.call<Pemantau>(instanceName: 'Pemantau Active').copyWith(hakInput: false);
+    _loadingController.sink.add(false);
   }
   bool get hakInputPemantau => locator
     .get<Pemantau>(instanceName: 'Pemantau Active').hakInput;
