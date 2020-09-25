75ec3c1785ada6c4fe4905220737c6359f191af9
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-25 19:03:16 +0700

    fix and update auth service

--- lib/service/service_auth.dart
+++ lib/service/service_auth.dart
@@ -3,76 +3,76 @@ part of 'service.dart';
 class AuthService {
   final fire_auth.FirebaseAuth _auth = fire_auth.FirebaseAuth.instance;
   
-  bool _isSignIn = false;
-  bool get isSignIn => _isSignIn;
+  // bool _isSignIn = false;
+  // bool get isSignIn => _isSignIn;
 
-  Future<void> init() async {
-    final fire_auth.User authUser = await _auth.authStateChanges().first;
-    _isSignIn = authUser != null;
+  // Future<void> init() async {
+  //   final fire_auth.User authUser = await _auth.authStateChanges().first;
+  //   _isSignIn = authUser != null;
 
-    UserService _userService;
+  //   UserService _userService;
     
-    if (_isSignIn) {
-      final TypeUser type = await UserService.findType(authUser.uid);
+  //   if (_isSignIn) {
+  //     final TypeUser type = await UserService.findType(authUser.uid);
       
-      if (type == TypeUser.admin) {
-        _userService = locator.get<UserService>(instanceName: 'Service Admin');
-      } else if (type == TypeUser.pemantau) {
-        _userService = locator.get<UserService>(instanceName: 'Service Pemantau');
-      }
+  //     if (type == TypeUser.admin) {
+  //       _userService = locator.get<UserService>(instanceName: 'Service Admin');
+  //     } else if (type == TypeUser.pemantau) {
+  //       _userService = locator.get<UserService>(instanceName: 'Service Pemantau');
+  //     }
 
-      final User user = await _userService.getUser(authUser.uid);
-      locator.call<User>(instanceName: 'User Active').duplicate(user);
-    }
-  }
+  //     final User user = await _userService.getUser(authUser.uid);
+  //     locator.call<User>(instanceName: 'User Active').duplicate(user);
+  //   }
+  // }
 
-  Future<AuthResult> signUp(String name, String email, String password, TypeUser type) async {
+  // Future<AuthResult> signUp(String name, String email, String password, TypeUser type) async {
 
-    try {
-      final fire_auth.UserCredential result = await _auth.createUserWithEmailAndPassword(
-        email: email, password: password);
+  //   try {
+  //     final fire_auth.UserCredential result = await _auth.createUserWithEmailAndPassword(
+  //       email: email, password: password);
 
-      UserService _userService;
-      if (type == TypeUser.admin) {
-        _userService = UserService.admin();
-      } else if (type == TypeUser.pemantau) {
-        _userService = UserService.pemantau();
-      }
+  //     UserService _userService;
+  //     if (type == TypeUser.admin) {
+  //       _userService = UserService.admin();
+  //     } else if (type == TypeUser.pemantau) {
+  //       _userService = UserService.pemantau();
+  //     }
 
-      User user;
-      if (type == TypeUser.admin) {
-        await _userService.createUser(Admin(
-          result.user.uid,
-          name,
-          email,
-        ));  
-      } else if (type == TypeUser.pemantau) {
-        await _userService.createUser(Pemantau(
-          result.user.uid,
-          name,
-          email,
-        ));
-      }
+  //     User user;
+  //     if (type == TypeUser.admin) {
+  //       await _userService.createUser(Admin(
+  //         result.user.uid,
+  //         name,
+  //         email,
+  //       ));  
+  //     } else if (type == TypeUser.pemantau) {
+  //       await _userService.createUser(Pemantau(
+  //         result.user.uid,
+  //         name,
+  //         email,
+  //       ));
+  //     }
 
-      return AuthResult(user: user);
+  //     return AuthResult(user: user);
     
-    } on fire_auth.FirebaseAuthException catch (e) {
+  //   } on fire_auth.FirebaseAuthException catch (e) {
       
-      String errorType = '';
+  //     String errorType = '';
       
-      if (e.code == 'weak-password') {
-        errorType = 'Password is too weak';
-      }
-      else if (e.code == 'email-already-in-use') {
-        errorType = 'Email is already in use';
-      }
+  //     if (e.code == 'weak-password') {
+  //       errorType = 'Password is too weak';
+  //     }
+  //     else if (e.code == 'email-already-in-use') {
+  //       errorType = 'Email is already in use';
+  //     }
       
-      return AuthResult(message: errorType);
+  //     return AuthResult(message: errorType);
     
-    } catch (e) {
-      return AuthResult(message: 'Error signUp: $e');
-    }
-  }
+  //   } catch (e) {
+  //     return AuthResult(message: 'Error signUp: $e');
+  //   }
+  // }
 
   Future<AuthResult> signIn(String email, String password, TypeUser type) async {
     try {
