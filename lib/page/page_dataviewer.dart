861bc5c8d79fe84cf0cad1691070622587407440
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-25 21:31:11 +0700

    fix and update data viewer

--- lib/page/page_dataviewer.dart
+++ lib/page/page_dataviewer.dart
@@ -17,7 +17,7 @@ class DataViewerPage extends Page<DataViewerBloc> {
       ),
       body: Builder(
         builder: (contextScaffold) => FutureBuilder<List<Calon>>(
-          future: bloc.getCalons(),
+          future: _bloc.getCalons(),
           builder: (context, snapshot) => (snapshot.hasData) ? SingleChildScrollView(
             padding: const EdgeInsets.symmetric(horizontal: 30),
             child: Column(
@@ -26,7 +26,7 @@ class DataViewerPage extends Page<DataViewerBloc> {
                 PieChart(
                   PieChartData(
                     centerSpaceRadius: 0,
-                    sectionsSpace: 5,
+                    sectionsSpace: 0,
                     sections: showingSections(),
                     borderData: FlBorderData(
                       show: false,
@@ -39,48 +39,54 @@ class DataViewerPage extends Page<DataViewerBloc> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text('Total Pemilih', style: blackContentRegular,),
-                      Text(convertCurr(bloc.totalSuara()), style: blackNumber,)
+                      Text(convertCurr(_bloc.totalSuara()), style: blackNumber,)
                     ],
                   ),
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(height: 15,),
                 XBox(
                   borderColor: borderColor,
                   child: Row(
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Suara Sah', style: blackContentRegular,),
-                          Text(convertCurr(bloc.totalSuaraSah()), style: blackNumber,)
-                        ],
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Suara Sah', style: blackContentRegular,),
+                            Text(convertCurr(_bloc.totalSuaraSah()), style: blackNumber,)
+                          ],
+                        ),
                       ),
-                      const SizedBox(width: 30,),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Suara Tidak Sah', style: blackContentRegular,),
-                          Text(convertCurr(bloc.totalSuaraTidakSah()), style: blackNumber,)
-                        ],
+                      const Expanded(child: SizedBox(width: 30,),),
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Suara Tidak Sah', style: blackContentRegular,),
+                            Text(convertCurr(_bloc.totalSuaraTidakSah()), style: blackNumber,)
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(height: 15,),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: List.generate(bloc.calons.length, (index) => XBox(
+                  children: List.generate(_bloc.calons.length, (index) => XBox(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('Calon ${bloc.calons[index].number}', style: blackSubtitleRegular,),
-                        Text('${bloc.calons[index].totalSuara}', style: blackNumber,)
+                        Text('Calon ${_bloc.calons[index].number}', style: blackSubtitleRegular,),
+                        Text('${_bloc.calons[index].totalSuara}', style: blackNumber,)
                       ],
                     ),
                   ))
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(height: 15,),
               ],
             ),
           ) : const Center(child: CircularProgressIndicator(),)
@@ -90,12 +96,17 @@ class DataViewerPage extends Page<DataViewerBloc> {
   }
 
   List<PieChartSectionData> showingSections() {
-    return List.generate(3,
+    final List<Color> colors = [mainColor, mainColor.withOpacity(0.75), mainColor.withOpacity(0.5)];
+    
+    final List<Calon> sortedCalon = _bloc.calons;
+    sortedCalon.sort((item2, item1) => item1.totalSuara.compareTo(item2.totalSuara));
+
+    return List.generate(_bloc.calons.length,
       (i) => PieChartSectionData(
-        color: mainColor,
-        value: 20.0 * (i+1),
-        radius: 100,
-        title: '',
+        color: colors[i],
+        value: sortedCalon[i].totalSuara.toDouble(),
+        radius: (120 - ((i+1)*5)).toDouble(),
+        title: '${sortedCalon[i].number}',
       )
     );
   }
