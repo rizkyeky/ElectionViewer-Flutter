ec4abdaef7e3fb489a35a3fb79b6effbfbca865d
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-25 19:03:42 +0700

    fix and update calon service

--- lib/service/service_calon.dart
+++ lib/service/service_calon.dart
@@ -5,30 +5,13 @@ class CalonService {
   final fire_store.CollectionReference _calonCollection = fire_store.FirebaseFirestore
     .instance.collection('calon'); 
 
-  Future<void> createCalon(Calon calon) async {
-    await _calonCollection.doc(calon.id).set({
-      'nama': calon.name,
-      'nomor': calon.number,
-      'total_suara': calon.totalSuara,
-      'suara_sah': calon.sahSuara,
-      'suara_tidak_sah': calon.tidaksahSuara
-    });
-  }
-
-  Future<void> updateCalon(String id, {String email, String name, String photoURL, 
-  int totalSuara, int sahSuara, int tidaksahSuara}) async {
+  Future<void> updateCalon(String id, {int totalSuara, int sahSuara, int tidaksahSuara}) async {
 
     final fire_store.DocumentSnapshot snapshot = await _calonCollection
       .doc(id).get();
     
     final Map<String, dynamic> data = {};
 
-    if (email != null) {
-      data['email'] = email;
-    }
-    if (name != null) {
-      data['nama'] = name;
-    }
     if (totalSuara != null) {
       data['total_suara'] = totalSuara + (snapshot.data()['total_suara'] as num).toInt();
     }
@@ -42,17 +25,16 @@ class CalonService {
     await _calonCollection.doc(id).update(data);
   }
 
-  Future<List<Calon>> getCalons() async {
+  Future<List<Calon>> getCalons({bool encrypt = true}) async {
     final fire_store.QuerySnapshot snapshot = await _calonCollection.get();
     
     final List<Calon> data = snapshot.docs.map((e) => Calon(
       e.id,
       e.data()["nama"] as String,
       (e.data()["nomor"] as num).toInt(),
-      totalSuara: (e.data()["total_suara"] as num).toInt(),
-      sahSuara: (e.data()["suara_sah"] as num).toInt(),
-      tidaksahSuara: (e.data()["suara_tidak_sah"] as num).toInt(),
-      photoURL: e.data()["photoURL"] as String,
+      totalSuara: encrypt ? 0 : (e.data()["total_suara"] as num).toInt(),
+      sahSuara: encrypt ? 0 : (e.data()["suara_sah"] as num).toInt(),
+      tidaksahSuara: encrypt ? 0 : (e.data()["suara_tidak_sah"] as num).toInt(),
     )).toList();
 
     return data;
@@ -69,7 +51,6 @@ class CalonService {
       totalSuara: (snapshot.data()["total_suara"] as num).toInt(),
       sahSuara: (snapshot.data()["suara_sah"] as num).toInt(),
       tidaksahSuara: (snapshot.data()["suara_tidak_sah"] as num).toInt(),
-      photoURL: snapshot.data()["photoURL"] as String,
     );
   }
 
