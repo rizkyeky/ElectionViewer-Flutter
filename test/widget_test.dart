6231c94d5f5b6c8cfdea60dbeeb0c91137669531
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-09-25 19:01:23 +0700

    add package crypto

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,10 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 // import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
 import 'package:flutter_test/flutter_test.dart';
 // import 'package:pemilihan_app/model/model.dart';
-
 // import 'package:pemilihan_app/service/service.dart';
 
 // import 'package:pemilihan_app/main.dart';
@@ -24,4 +25,13 @@ Future<void> main() async {
   //   final List<Kelurahan> listKel = await _serviceKelu.getKelurahans(kec.id);
   //   listKel.forEach((element) {print('-${element.name}');});
   // }
+  var bytes = utf8.encode(100.toString()); // data being hashed
+
+  var md5Convert = md5.convert(bytes);
+  // var sha1Convert = sha1.convert(bytes);
+
+  // print("Digest as bytes: $bytes");
+  print("md5Convert as hex string: $md5Convert");
+  // print("sha1Convert as hex string: $sha1Convert");
+  // print("margerConvert as hex string: $margerConvert");
 }
